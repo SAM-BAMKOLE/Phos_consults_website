e38edebd525f3bf4050eeced5478b4e505eec9e1
samBamkole <olagoroyesamuel7@gmail.com>
2023-06-05 18:04:06 +0100

    modified index page and started about page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./*.html", "./pages/*.html", "./node_modules/flowbite/**/*.js"],
+    content: [
+        "./pages/**/*.html",
+        "./about.html",
+        "./index.html",
+        "./node_modules/flowbite/**/*.js",
+    ],
     theme: {
         screens: {
-      'sm': '640px',
-      // => @media (min-width: 640px) { ... }
+            sm: "640px",
+            // => @media (min-width: 640px) { ... }
 
-      'md': '768px',
-      // => @media (min-width: 768px) { ... }
+            md: "768px",
+            // => @media (min-width: 768px) { ... }
 
-      'lg': '1024px',
-      // => @media (min-width: 1024px) { ... }
+            lg: "1024px",
+            // => @media (min-width: 1024px) { ... }
 
-      'xl': '1280px',
-      // => @media (min-width: 1280px) { ... }
+            xl: "1280px",
+            // => @media (min-width: 1280px) { ... }
 
-      '2xl': '1536px',
-      // => @media (min-width: 1536px) { ... }
-    },
+            "2xl": "1536px",
+            // => @media (min-width: 1536px) { ... }
+        },
         extend: {
             backgroundImage: {
                 testimonial:
                     " linear-gradient(rgba(255, 255, 255, 0.65), rgba(255, 255, 255, 0.65)), url('/images/testimonial-background.jpg')",
                 addressHero:
                     " linear-gradient(rgba(0, 0, 0, 0.75), rgba(3, 7, 18, 0.75))",
+                pattern:
+                    " linear-gradient(to bottom, rgba(255, 255, 255, 0.7), rgba(255, 255, 255, 0.7), rgba(255, 255, 255, 1)), url('/images/pattern-2.jpg')",
             },
             fontFamily: {
                 raleway: [' "Raleway", sans-serif; '],
